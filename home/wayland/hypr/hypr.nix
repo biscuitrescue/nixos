34694ef9dba018cb8be0db9c8a9b59313db0cd83
Karttikeya Sinha <biscuitrescue>
2025-06-10 16:40:05 +0530

    10th ws on hypr

--- home/wayland/hypr/hypr.nix
+++ home/wayland/hypr/hypr.nix
@@ -167,82 +167,8 @@ animations {
               "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
             ]
           )
-            9)
+            10)
         );
     };
   };
-  # programs.hyprlock = {
-  #   enable = true;
-  #   settings = {
-  #     general = {
-  #       # disable_loading_bar = true;
-  #       grace = 200;
-  #       hide_cursor = true;
-  #       # no_fade_in = false;
-  #     };
-  #
-  #     background = [
-  #       {
-  #         monitor = "eDP-1";
-  #         path = "screenshot";
-  #         blur_passes = 1; # 0 disables blurring
-  #         blur_size = 7;
-  #         noise = 1.17e-2;
-  #       }
-  #     ];
-  #     label = [
-  #       {
-  #         monitor = "DP-2";
-  #         text = "$TIME";
-  #         color = "rgba(242, 243, 244, 0.75)";
-  #         font_size = 95;
-  #         font_family = "JetBrainsMono Nerd Font";
-  #         position = "0, 300";
-  #         halign = "center";
-  #         valign = "center";
-  #       }
-  #     ];
-      # background = [
-      #   {
-      #     path = "screenshot";
-      #     blur_passes = 3;
-      #     blur_size = 8;
-      #   }
-      # ];
-
-      # input-field = [
-      #   {
-      #     size = "200, 50";
-      #     position = "0, -80";
-      #     monitor = "";
-      #     dots_center = true;
-      #     fade_on_empty = false;
-      #     font_color = "rgb(202, 211, 245)";
-      #     inner_color = "rgb(91, 96, 120)";
-      #     outer_color = "rgb(24, 25, 38)";
-      #     outline_thickness = 5;
-      #     shadow_passes = 2;
-      #   }
-      # ];
-  #     input-field = {
-  #       monitor = "eDP-1";
-  #       size = "200,50";
-  #       outline_thickness = 2;
-  #       dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
-  #       dots_spacing = 0.35; # Scale of dots' absolute size, 0.0 - 1.0
-  #       dots_center = true;
-  #       outer_color = "rgba(0, 0, 0, 0)";
-  #       inner_color = "rgba(0, 0, 0, 0.2)";
-  #       font_color = "rgb(111, 45, 104)";
-  #       fade_on_empty = false;
-  #       rounding = -1;
-  #       check_color = "rgb(30, 107, 204)";
-  #       placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
-  #       hide_input = false;
-  #       position = "0, -100";
-  #       halign = "center";
-  #       valign = "center";
-  #     };
-  #   };
-  # };
 }
