668d9cff968236c516129f47779a5d7157fda13c
Karttikeya Sinha <biscuitrescue>
2025-06-10 13:34:20 +0530

    hypridle -> swayidle

--- home/wayland/mako.nix
+++ home/wayland/mako.nix
@@ -6,10 +6,11 @@
     settings = {
       background-color = "#181616";
       border-color = "#aed1dc";
+      progress-color = "#8a9a7b";
       border-radius = 10;
       border-size = 2;
       default-timeout = 2500;
-      font = "JetbrainsMono Nerd Font 13";
+      font = "M Plus 1 Code Nerd Font 13";
       anchor = "top-center";
       margin = 10;
       icons = true;
