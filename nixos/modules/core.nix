cf1a6234a61d927bcb0ed58c5a808fbacb886475
biscuitrescue <karttikeyasinha11@gmail.com>
2025-09-08 14:30:04 +0530

    openvpn boilerplate

--- nixos/modules/core.nix
+++ nixos/modules/core.nix
@@ -46,68 +46,6 @@
     LC_TIME = "en_IN";
   };
 
-  services = {
-    # logind.lidSwitch = "ignore";
-
-    cloudflare-warp.enable = true;
-    gvfs.enable = true;
-    samba.enable = true;
-    dbus.enable = true;
-
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 80;
-
-        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-
-      };
-    };
-    blueman.enable = true;
-    libinput = {
-      enable = true;
-      touchpad = {
-        naturalScrolling = true;
-        tapping = true;
-      };
-    };
-    xserver = {
-      enable = true;
-      displayManager = {
-        startx.enable = true;
-      };
-      xkb = {
-        layout = "us";
-        variant = "";
-      };
-    };
-
-    gnome.gnome-keyring.enable = true;
-    openssh = {
-      enable = true;
-      settings.PasswordAuthentication = false;
-      ports = [ 22 443 ];
-    };
-    tailscale.enable = true;
-    printing.enable = false;
-    pulseaudio.enable = false;
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-    };
-  };
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
